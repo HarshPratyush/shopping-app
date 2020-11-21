ee68ec2c1c21bab2fabbbec5b349feca224ffc18
Harsh Pratyush <harshpratyush1206@gmail.com>
2020-11-21 23:45:47 +0530

    Renamed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'altrooist-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'shopping-app';
+  title = 'altrooist-app';
 }
